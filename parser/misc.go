3ff0aca79e1ad5af3e48ed8e6d92e70ce63dc1bf
zimulala <zimu_xia@126.com>
2017-08-24 14:30:26 +0800

    *: implement "admin show ddl jobs"

--- parser/misc.go
+++ parser/misc.go
@@ -542,6 +542,7 @@ var tokenMap = map[string]int{
 	"LONGTEXT":                   longtextType,
 	"BOOL":                       boolType,
 	"BOOLEAN":                    booleanType,
+	"JOBS":                       jobs,
 	"JSON":                       jsonType,
 	"JSON_EXTRACT":               jsonExtract,
 	"JSON_UNQUOTE":               jsonUnquote,
