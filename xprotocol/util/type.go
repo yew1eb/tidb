e653f0ecf35a6ad5d43e725eeb85d76738f142a8
Yu Shuaipeng <jackysp@gmail.com>
2017-09-15 16:55:42 +0800

    fix type error

--- xprotocol/util/type.go
+++ xprotocol/util/type.go
@@ -1,9 +1,11 @@
 package util
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/juju/errors"
 	"github.com/pingcap/tidb/mysql"
 	"github.com/pingcap/tipb/go-mysqlx/Resultset"
+	"github.com/pingcap/tidb/util/types"
 )
 
 var unsignedXType = map[byte]Mysqlx_Resultset.ColumnMetaData_FieldType{
@@ -13,6 +15,34 @@ var unsignedXType = map[byte]Mysqlx_Resultset.ColumnMetaData_FieldType{
 	mysql.TypeInt24:    Mysqlx_Resultset.ColumnMetaData_UINT,
 	mysql.TypeLong:     Mysqlx_Resultset.ColumnMetaData_UINT,
 	mysql.TypeLonglong: Mysqlx_Resultset.ColumnMetaData_UINT,
+
+	// TODO: Remove the following types when TiDB deals with unsigned flag properly.
+	// Clarified type
+	mysql.TypeDouble:    Mysqlx_Resultset.ColumnMetaData_DOUBLE,
+	mysql.TypeFloat:     Mysqlx_Resultset.ColumnMetaData_FLOAT,
+	mysql.TypeDecimal:   Mysqlx_Resultset.ColumnMetaData_DECIMAL,
+	mysql.TypeVarchar:   Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeString:    Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeGeometry:  Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeDuration:  Mysqlx_Resultset.ColumnMetaData_TIME,
+	mysql.TypeDate:      Mysqlx_Resultset.ColumnMetaData_DATETIME,
+	mysql.TypeDatetime:  Mysqlx_Resultset.ColumnMetaData_DATETIME,
+	mysql.TypeYear:      Mysqlx_Resultset.ColumnMetaData_UINT,
+	mysql.TypeTimestamp: Mysqlx_Resultset.ColumnMetaData_DATETIME,
+	mysql.TypeSet:       Mysqlx_Resultset.ColumnMetaData_SET,
+	mysql.TypeEnum:      Mysqlx_Resultset.ColumnMetaData_ENUM,
+	mysql.TypeNull:      Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeBit:       Mysqlx_Resultset.ColumnMetaData_BIT,
+
+	// TODO: Clarify type mapping below.
+	mysql.TypeNewDate:    Mysqlx_Resultset.ColumnMetaData_DATETIME,
+	mysql.TypeJSON:       Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeNewDecimal: Mysqlx_Resultset.ColumnMetaData_DECIMAL,
+	mysql.TypeTinyBlob:   Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeMediumBlob: Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeLongBlob:   Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeBlob:       Mysqlx_Resultset.ColumnMetaData_BYTES,
+	mysql.TypeVarString:  Mysqlx_Resultset.ColumnMetaData_BYTES,
 }
 
 var commonXType = map[byte]Mysqlx_Resultset.ColumnMetaData_FieldType{
@@ -61,5 +91,6 @@ func Mysql2XType(tp byte, unsigned bool) (*Mysqlx_Resultset.ColumnMetaData_Field
 			return &colTp, nil
 		}
 	}
-	return nil, errors.New("unknown column type")
+	log.Infof("[YUSP] %v", unsigned)
+	return nil, errors.Errorf("unknown column type %s", types.TypeStr(tp))
 }
